f0b25548bcefe315f564d4fc79ded6912ab88c07
Simon Schwab <schw4b@gmail.com>
2016-04-07 16:42:53 +0100

    More unit tests

--- devel/devel.R
+++ devel/devel.R
@@ -1,8 +1,42 @@
 # 2016 Simon Schwab, s.schwab@warwick.ac.uk
 # University of Warwick, United Kingdom
+#
+# developer script to for unit testing, profiling etc.
 
-library(compiler)
-source("~/workspace/mdm/R/mdm.R")
+# install packages ----
+install.packages("microbenchmark")
+install.packages("devotools")
+
+setwd('~/workspace')
+
+library(devtools)
+library(microbenchmark)
+load_all('mdm')
+# unload("mdm/")
+
+# Run unit tests ----
+library(testthat)
+test_dir('mdm/tests', reporter = 'Summary')
+
+# Benchmark ----
+n=1;   # node to test
+p=2:5; # parent node
+nP=length(p);  # n parents
+
+data("utestdata")
+Yt = myts[,n]
+Ft=array(1,dim=c(200,nP+1))
+Ft[,2:ncol(Ft)]=myts[,p]
+
+microbenchmark(dlm.filt.rh(Yt,t(Ft),0.93),dlm.filt(Yt,t(Ft),0.93))
+microbenchmark(dlm.filt(Yt,t(Ft),0.93),       # 4 parents
+               dlm.filt(Yt,t(Ft[,1:4]),0.93), # 3
+               dlm.filt(Yt,t(Ft[,1:3]),0.93), # 2
+               dlm.filt(Yt,t(Ft[,1:2]),0.93)) # 1 Effect of 1 to 5 nodes is 10%
+# cmpfun     200  50 n of timepoints
+# 4 parents  40%  41% reduction in time
+# 1 parent   40%  44%
+
 
 # Profiling ----
 Rprof("profile.out")
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -5,6 +5,10 @@ context("Running MDM functions")
 
 test_that("Running filtering distribution: 1 parent", {
   
+  n=1;   # node to test
+  p=2;   # parent node(s)
+  nP=length(p);  # n parents
+  
   # # initial preparation of true values to test against later on
   # setwd("~/Data/NetSim/sim/smith-nodes/")
   # library(R.matlab)
@@ -13,11 +17,9 @@ test_that("Running filtering distribution: 1 parent", {
   # rm(d)
   
   data("utestdata")
-  Yt = myts[,1]
-  expect_that(length(Yt), equals(200))
-  
-  Ft=array(1,dim=c(200,2))
-  Ft[,2:ncol(Ft)]=myts[,2]
+  Yt = myts[,n]
+  Ft=array(1,dim=c(200,nP+1))
+  Ft[,2:ncol(Ft)]=myts[,p]
   
   # # initial preparation of true values to test against later on
   # a=dlm.filt(Yt,t(Ft),0.93)
@@ -28,8 +30,80 @@ test_that("Running filtering distribution: 1 parent", {
   # save(myts, utestdata, file = "utestdata.RData")
 
   a=dlm.filt(Yt,t(Ft),0.93)
-  expect_that(length(a$lpl), equals(200))
+  #expect_that(length(a$lpl), equals(200))
   expect_that(sum(a$lpl[15:200]), equals(utestdata$Np1.lplsum))
   expect_that(a$lpl[15:200], equals(utestdata$Np1.lpl[15:200]))
 
 })
+
+test_that("Running filtering distribution: 2 parents", {
+  
+  # sample(1:5,5, replace=F) # which node should i test?
+  n=3;   # node to test
+  p=c(1,4); # parent node
+  nP=length(p);  # n parents
+  
+  data("utestdata")
+  Yt = myts[,n]
+  Ft=array(1,dim=c(200,nP+1))
+  Ft[,2:ncol(Ft)]=myts[,p]
+  a=dlm.filt(Yt,t(Ft),0.93)
+  
+  # # initial preparation of true values to test against later on
+  # utestdata$Np2.lpl=a$lpl
+  # utestdata$Np2.lplsum=sum(a$lpl[15:200])
+  # setwd("mdm/data/")
+  # save(myts, utestdata, file = "utestdata.RData")
+  
+  expect_that(sum(a$lpl[15:200]), equals(utestdata$Np2.lplsum))
+  expect_that(a$lpl[15:200], equals(utestdata$Np2.lpl[15:200]))
+  
+})
+
+test_that("Running filtering distribution: 3 parents", {
+  
+  # sample(1:5,5, replace=F) # which node should i test?
+  n=4;   # node to test
+  p=c(1,2,3); # parent node
+  nP=length(p);  # n parents
+  
+  data("utestdata")
+  Yt = myts[,n]
+  Ft=array(1,dim=c(200,nP+1))
+  Ft[,2:ncol(Ft)]=myts[,p]
+  a=dlm.filt(Yt,t(Ft),0.93)
+  
+  # # initial preparation of true values to test against later on
+  # utestdata$Np3.lpl=a$lpl
+  # utestdata$Np3.lplsum=sum(a$lpl[15:200])
+  # setwd("mdm/data/")
+  # save(myts, utestdata, file = "utestdata.RData")
+  
+  expect_that(sum(a$lpl[15:200]), equals(utestdata$Np3.lplsum))
+  expect_that(a$lpl[15:200], equals(utestdata$Np3.lpl[15:200]))
+  
+})
+
+test_that("Running filtering distribution: 4 parents", {
+  
+  n=1;   # node to test
+  p=2:5; # parent node
+  nP=length(p);  # n parents
+  
+  data("utestdata")
+  Yt = myts[,n]
+  Ft=array(1,dim=c(200,nP+1))
+  Ft[,2:ncol(Ft)]=myts[,p]
+  a=dlm.filt(Yt,t(Ft),0.93)
+  
+  # # initial preparation of true values to test against later on
+  # utestdata$Np4.lpl=a$lpl
+  # utestdata$Np4.lplsum=sum(a$lpl[15:200])
+  # setwd("mdm/data/")
+  # save(myts, utestdata, file = "utestdata.RData")
+  
+  expect_that(sum(a$lpl[15:200]), equals(utestdata$Np4.lplsum))
+  expect_that(a$lpl[15:200], equals(utestdata$Np4.lpl[15:200]))
+  
+})
+
